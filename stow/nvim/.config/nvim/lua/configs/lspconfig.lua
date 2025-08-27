3519336daa59c72bf48d8a1807fe8f4447c033fe
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-08-27 07:02:39 +0300

    chore(nvim): remove zls and elp from lspconfig

--- stow/nvim/.config/nvim/lua/configs/lspconfig.lua
+++ stow/nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -2,15 +2,9 @@ local nvlsp = require "nvchad.configs.lspconfig"
 
 nvlsp.defaults()
 
-local servers = { "ts_ls", "pyright", "ccls", "rust_analyzer", "elp" }
+local servers = { "ts_ls", "pyright", "ccls", "rust_analyzer" }
 
 vim.lsp.enable(servers)
-
-vim.lsp.config("zls", {
-  on_init = function() end,
-  on_attach = nvlsp.on_attach,
-  capabilities = nvlsp.capabilities,
-})
 
 vim.lsp.config("rust_analyzer", {
   on_init = function() end,
