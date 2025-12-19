650059b45f3a6679c73badd41da672755888e153
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-12-19 16:51:28 +0200

    customize lualine sections

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -170,6 +170,22 @@ require("lualine").setup {
         section_separators = { left = "", right = "" },
         component_separators = { left = "", right = "" },
     },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
 }
 
 require("nvim-autopairs").setup {}
